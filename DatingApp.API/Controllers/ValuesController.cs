35c6a1967f66d2f5250a4848543ed96a20d22b2c
Emi Minoiu <minoiu_emi@yahoo.com>
2019-07-13 22:38:48 +0100

    Added my project

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+  
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+       private readonly DataContext _context;
+       public ValuesController(DataContext _context)
+       {
+           this._context = _context;
+       }
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var val = _context.Values.FirstOrDefault(v => v.Id == id);
+            return Ok(val);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
